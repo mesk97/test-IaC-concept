1fbb6cf7863f81955a49a5fa5f08378872782684
Konstantin Pakulin <mesk@acronis.com>
2022-12-16 10:37:04 +0200

    comments

--- memory/addr.c
+++ memory/addr.c
@@ -4,11 +4,15 @@
 #include <unistd.h>
 #include <string.h>
 
+// Это функция - ее код находится в сегменте "Код/Code"
 int main (int argc, char ** argv)
 {
+	// Это локальная перменная функции - она находится в сегменте "Стэк/Stack"
 	int v = 3;
+
 	printf("Code  is at %p \n", (void *)main);
-	printf("Heap  is at %p \n", malloc(8));
+	printf("Heap  is at %p \n", 
+		malloc(8)); // Malloc выделяет память в сегмента "Куча/Heap"
 	printf("Stack is at %p \n", (void *)&v);
 
 	sleep (100);
